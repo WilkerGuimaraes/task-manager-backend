4eb159e0663b81cb146ca166b1dbd1de80c191c3
Wilker Guimaraes <wilkerguimaraes.07@gmail.com>
2024-04-08 11:57:18 -0300

    feat: add new route to create new Task to MongoDB database

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,31 @@
 import fastify from "fastify";
+import { z } from "zod";
+import { prisma } from "./database/prisma-database.js";
 
 const app = fastify();
 
 app.get("/", () => {
   return "Hello World!";
+});
+
+app.post("/tasks", async (request, reply) => {
+  try {
+    const createNewTask = z.object({
+      description: z.string(),
+    });
+
+    const data = createNewTask.parse(request.body);
+
+    const task = await prisma.task.create({
+      data: {
+        description: data.description,
+      },
+    });
+
+    return reply.status(201).send(task);
+  } catch (error) {
+    return reply.status(500).send("Internal Server Error");
+  }
 });
 
 app
